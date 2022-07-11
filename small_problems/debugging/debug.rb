041c7813a291d07d6e4e72ce04ce10f160c136f8
EC2 Default User <ec2-user@ip-172-31-86-8.ec2.internal>
2022-07-11 18:02:04 +0000

    debug

--- small_problems/debugging/debug.rb
+++ small_problems/debugging/debug.rb
@@ -60,39 +60,140 @@ p reverse_sentence('how are you doing')
 
 
 
-cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
+# cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
 
-deck = { :hearts   => cards,
-         :diamonds => cards,
-         :clubs    => cards,
-         :spades   => cards }
+# deck = { :hearts   => cards,
+#         :diamonds => cards,
+#         :clubs    => cards,
+#         :spades   => cards }
 
-def score(card)
-  case card
-  when :ace   then 11
-  when :king  then 10
-  when :queen then 10
-  when :jack  then 10
-  else card
+# def score(card)
+#   case card
+#   when :ace   then 11
+#   when :king  then 10
+#   when :queen then 10
+#   when :jack  then 10
+#   else card
+#   end
+# end
+
+# # Pick one random card per suit
+
+# player_cards = []
+# deck.keys.each do |suit|
+#   cards = deck[suit].shuffle!
+#   player_cards << cards.pop
+# end
+
+# # Determine the score of the remaining cards in the deck
+
+# sum = deck.reduce(0) do |sum, (_, remaining_cards)|
+#   scores = remaining_cards.map do |card|
+#               score(card)
+#             end
+
+#   sum + scores.sum
+# end
+
+# puts sum
+
+
+#GETTING THINGS DONE
+
+
+# def move(n, from_array, to_array)
+#   return if n == 0
+#   to_array << from_array.shift
+#   move(n - 1, from_array, to_array)
+# end
+
+# # Example
+
+# todo = ['study', 'walk the dog', 'coffee with Tom']
+# done = ['apply sunscreen', 'go to the beach']
+
+# move(2, todo, done)
+
+# p todo # should be: ['coffee with Tom']
+# p done #should be: ['apply sunscreen', 'go to the beach', 'study', 'walk the dog']
+
+
+# def neutralize(sentence)
+#   words = sentence.split(' ')
+  
+#   words.select{ |word| !negative?(word) }.join(' ')
+# end
+
+# def negative?(word)
+#   [ 'dull',
+#     'boring',
+#     'annoying',
+#     'chaotic'
+#   ].include?(word)
+# end
+
+# puts neutralize('These dull boring cards are part of a chaotic board game.')
+# # Expected: These cards are part of a board game.
+# # Actual: These boring cards are part of a board game.
+
+
+
+# password = nil
+
+# def set_password
+#   puts 'What would you like your password to be?'
+#   gets.chomp
+# end
+
+# def verify_password(password)
+#   puts '** Login **'
+#   print 'Password: '
+#   input = gets.chomp
+
+#   if input == password
+#     puts 'Welcome to the inside!'
+#   else
+#     puts 'Authentication failed.'
+#   end
+# end
+
+
+
+# if !password
+#   password = set_password
+# end
+
+# verify_password(password)
+
+
+def valid_integer?(string)
+  string.to_i.to_s == string
+end
+
+def guess_number(max_number, max_attempts)
+  winning_number = (1..max_number).to_a.sample
+  attempts = 0 
+  p winning_number
+  loop do
+    attempts += 1
+    break if attempts > max_attempts
+
+    input = nil
+    until valid_integer?(input)
+      print 'Make a guess: '
+      input = gets.chomp
+    end
+
+    guess = input.to_i
+
+    if guess == winning_number
+      puts 'Yes! You win.'
+      break
+    else
+      puts 'Nope. Too small.' if guess < winning_number
+      puts 'Nope. Too big.'   if guess > winning_number
+      end
   end
 end
 
-# Pick one random card per suit
-
-player_cards = []
-deck.keys.each do |suit|
-  cards = deck[suit].shuffle!
-  player_cards << cards.pop
-end
-
-# Determine the score of the remaining cards in the deck
-
-sum = deck.reduce(0) do |sum, (_, remaining_cards)|
-   scores = remaining_cards.map do |card|
-              score(card)
-            end
-
-  sum + scores.sum
-end
-
-puts sum
+guess_number(10, 3)
